743943359eea3f5e64159bad119062b81b1f04fd
“Biel007O010” <bielflaquegrive@gmail.com>
2026-05-12 16:35:51 +0200

    APP

---
+++ src/prog2/vista/AppBiblioUB.java
@@ -0,0 +1,22 @@
+package prog2.vista;
+
+import javax.swing.*;
+
+public class AppBiblioUB extends JFrame {
+    private JPanel panellAppBiblioUB;
+
+    public AppBiblioUB(){
+        setTitle("Prova GUI");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panellAppBiblioUB);
+        setSize(500,400);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AppBiblioUB prova = new AppBiblioUB();
+            prova.setVisible(true);
+        });
+    }
+}
