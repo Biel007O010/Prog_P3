c03778b9bbb6e3197f67092f81cf7752e81b4170
eduar <eduardochoquediaz@gmail.com>
2026-05-24 17:25:53 +0200

    Terminat

--- src/prog2/vista/gestioPrestecsFinestra.java
+++ src/prog2/vista/gestioPrestecsFinestra.java
@@ -47,30 +47,9 @@ public class gestioPrestecsFinestra extends JFrame{
         retornarPrestecButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int prestecSeleccionat = llistaPrestecs.getSelectedIndex();
-
-                if (prestecSeleccionat == -1) {
-                    JOptionPane.showMessageDialog(gestioPrestecsFinestra.this,
-                            "Si us plau, selecciona un préstec de la llista per retornar-lo.",
-                            "Avís",
-                            JOptionPane.WARNING_MESSAGE);
-                    return;
-                }
-                try {
-                    adaptador.retornarPrestec(prestecSeleccionat);
-                    actualitzaPrestecs();
-
-                    JOptionPane.showMessageDialog(gestioPrestecsFinestra.this,
-                            "Préstec retornat correctament.",
-                            "Èxito",
-                            JOptionPane.INFORMATION_MESSAGE);
-
-                } catch (BiblioException ex) {
-                    JOptionPane.showMessageDialog(gestioPrestecsFinestra.this,
-                            ex.getMessage(),
-                            "Error al retornar",
-                            JOptionPane.ERROR_MESSAGE);
-                }
+                retornarPrestecFinestra finestraRetorn = new retornarPrestecFinestra(adaptador, gestioPrestecsFinestra.this);
+                finestraRetorn.setVisible(true);
+                actualitzaPrestecs();
             }
         });
     }
